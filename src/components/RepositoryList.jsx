265787fbee57b3902e1a381c0db8a44c192632db
Felipe Torres <felipetoress@gmail.com>
2021-12-07 17:09:37 -0300

    initial commit

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,25 @@
+import { RepositoryItem } from "./RepositoryItem";
+import {useState, useEffect} from 'react';
+
+import '../styles/repositories.scss';
+
+export function RepositoryList(){
+    const [repositories, setrepositories] = useState([]);
+    //1 argumento -qual funcao quero executar, 
+    //2 argumento- quando eu quero executar 
+    // [var] - quando a var mudar / [] - executar uma unica vez / vazio - ficar em loop
+    useEffect(()=>{
+        fetch('https://api.github.com/users/FelipeTorress/repos')
+            .then(response => response.json())
+                .then(data => setrepositories(data))
+    },[])
+
+    return (
+        <section className="repository-list">
+            <h1>Lista de Repositorios</h1>
+            <ul>
+                {repositories.map(repository => <RepositoryItem key={repository.name} repository = {repository}/>)}
+            </ul>
+        </section>
+    );
+}
