4814d62f8b7acec4d2c1f0dca25c3ecaca0f3a20
Felipe Torres <felipetoress@gmail.com>
2021-12-09 16:30:09 -0300

    mudando a aplicaçao para typescript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,8 +3,14 @@ import {useState, useEffect} from 'react';
 
 import '../styles/repositories.scss';
 
+interface Repository {
+    name: string;
+    description: string;
+    html_url: string;
+}
+
 export function RepositoryList(){
-    const [repositories, setrepositories] = useState([]);
+    const [repositories, setrepositories] = useState<Repository[]>([]);
     //1 argumento -qual funcao quero executar, 
     //2 argumento- quando eu quero executar 
     // [var] - quando a var mudar / [] - executar uma unica vez / vazio - ficar em loop
